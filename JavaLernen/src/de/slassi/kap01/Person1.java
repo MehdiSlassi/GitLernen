5d8cb16dc5f6dbe865b04ba54415ed3ee0909652
Slassi <mehdi_slassi@gmx.de>
2020-05-14 15:36:05 +0200

    Klasse Person1 hinzugefügt

---
+++ JavaLernen/src/de/slassi/kap01/Person1.java
@@ -0,0 +1,11 @@
+package de.slassi.kap01;
+
+ public class Person1 {
+	private float geld;
+	public Person1 (float geld){
+		this.geld = geld;
+	}
+ public float getGeld(Person1 p) {
+	return p.geld;
+ }
+}
