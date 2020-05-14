9384b996d00b6c45fe00fba8036f7e7a2b32d274
Slassi <mehdi_slassi@gmx.de>
2020-05-14 15:14:40 +0200

    Klassen PersonNeu und TestPerson bearbeitet

--- JavaLernen/src/de/slassi/kap01/PersonNeu.java
+++ JavaLernen/src/de/slassi/kap01/PersonNeu.java
@@ -1,11 +1,18 @@
 package de.slassi.kap01;
 
 public class PersonNeu {
-	public String name = "Mueller"; 
-	public int alter = 12;
+	private String name; 
+	private String vorname; 
+	private int alter;
 	
+	public PersonNeu(String name, String vorname, int alter) {
+		this.name = name;
+		this.vorname = vorname;
+		this.alter = alter;
+	}
 	public void print() {
 		System.out.println("Name : " + name);
+		System.out.println("Vorname : " + vorname);
 		System.out.println("Alter : " + alter);
 	}
 }
--- TestJavaLernen/src/de/slassi/testKap01/TestPersonNeu.java
+++ TestJavaLernen/src/de/slassi/testKap01/TestPersonNeu.java
@@ -7,7 +7,7 @@ public class TestPersonNeu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		PersonNeu p = new PersonNeu();
+		PersonNeu p = new PersonNeu("Mueller", "Frank", 20);
 		p.print();
 
 	}
