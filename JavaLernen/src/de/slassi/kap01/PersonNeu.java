d43db3ce7356291f0cafc3a34c3d9785a0b78cf3
Slassi <mehdi_slassi@gmx.de>
2020-05-14 14:51:25 +0200

    erstes Commit

---
+++ JavaLernen/src/de/slassi/kap01/PersonNeu.java
@@ -0,0 +1,11 @@
+package de.slassi.kap01;
+
+public class PersonNeu {
+	public String name = "Mueller"; 
+	public int alter = 12;
+	
+	public void print() {
+		System.out.println("Name : " + name);
+		System.out.println("Alter : " + alter);
+	}
+}
---
+++ TestJavaLernen/src/de/slassi/testKap01/TestPersonNeu.java
@@ -0,0 +1,15 @@
+package de.slassi.testKap01;
+
+import de.slassi.kap01.PersonNeu;
+
+public class TestPersonNeu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PersonNeu p = new PersonNeu();
+		p.print();
+
+	}
+
+}
