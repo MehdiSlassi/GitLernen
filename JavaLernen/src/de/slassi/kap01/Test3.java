7a631173446bcd457bfa0d937061a60e54eef1a8
Slassi <mehdi_slassi@gmx.de>
2020-07-26 10:50:53 +0200

    Test3.java ist geaendert

--- JavaLernen/src/de/slassi/kap01/Test3.java
+++ JavaLernen/src/de/slassi/kap01/Test3.java
@@ -5,6 +5,8 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Jetzt lerne ich Gitt diff");
+System.out.println("Jetzt lerne ich git Reset");
+System.out.println("Jetzt lerne ich git ResetTest");
 	}
 
 }
