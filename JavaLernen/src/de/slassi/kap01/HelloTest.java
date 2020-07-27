f31aa8137f5c7cd0d45f9b09f2a0100bbc63c854
Slassi <mehdi_slassi@gmx.de>
2020-07-27 18:26:50 +0200

    TestMergMaster

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -3,9 +3,9 @@ package de.slassi.kap01;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Testmergeneubranch");
-		System.out.println("Hello TestmergeMaster");
-		System.out.println("Testmergeneubranch");
+		
+		System.out.println("TestmergeMaster");
+		
 		
 	}
 
