b91ecbe29601e908674f0565c35c2f7a685b9015
Slassi <mehdi_slassi@gmx.de>
2020-07-27 15:06:03 +0200

    Merge

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -5,7 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World_Test!");
-		System.out.println("Hello World_Test!_neue Branch");
+		System.out.println("Hello Merge");
 	}
 
 }
