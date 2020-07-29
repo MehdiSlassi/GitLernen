a9afaa1a62a63c5935bace8b174b9619f1daec69
Slassi <mehdi_slassi@gmx.de>
2020-07-29 19:45:39 +0200

    kommitmerge

--- JavaLernen/src/de/slassi/kap01/TestRevert.java
+++ JavaLernen/src/de/slassi/kap01/TestRevert.java
@@ -6,6 +6,7 @@ public class TestRevert {
 		// TODO Auto-generated method stub
 		System.out.println("Jetzt lerne ich Revert");
 		System.out.println("Test Merge");
+		System.out.println("jetzt lerne ich ");
 	}
 
 }
