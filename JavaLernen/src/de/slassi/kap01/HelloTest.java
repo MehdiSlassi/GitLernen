6c827c57cb78d71f824381b8383a6f29ca4ff93f
Slassi <mehdi_slassi@gmx.de>
2020-08-01 12:22:52 +0200

    zweitePersonLerntrebase

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -4,9 +4,7 @@ public class HelloTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("TestmergeMaster");
-		System.out.println("Testbase");
-		
+		System.out.println("zweitePerson_lernt_rebase");
 		
 	}
 
