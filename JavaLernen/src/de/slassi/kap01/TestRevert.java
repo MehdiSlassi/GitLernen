b9f1574ea24cdc320a36d92d1ab9b589939e647d
Slassi <mehdi_slassi@gmx.de>
2020-08-01 12:23:52 +0200

    erstePersonlerntrebease

--- JavaLernen/src/de/slassi/kap01/TestRevert.java
+++ JavaLernen/src/de/slassi/kap01/TestRevert.java
@@ -3,12 +3,8 @@ package de.slassi.kap01;
 public class TestRevert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Jetzt lerne ich Revert");
-		System.out.println("Test Merge");
-		System.out.println("jetzt lerne ich ");
-		System.out.println("jetzt lerne ich den Befehl amend ");
-		System.out.println("jetzt lerne ich den rebease");
+		;
+		System.out.println("erstePerson lernt rebease");
 		
 	}
 
