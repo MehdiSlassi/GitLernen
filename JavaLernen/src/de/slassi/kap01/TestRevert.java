6657faa5f55a2773eb0dd8d63e7c5cf6e7fb2a31
Slassi <mehdi_slassi@gmx.de>
2020-08-05 17:45:58 +0200

    erstepersonlerntrebasewieder

--- JavaLernen/src/de/slassi/kap01/TestRevert.java
+++ JavaLernen/src/de/slassi/kap01/TestRevert.java
@@ -5,6 +5,7 @@ public class TestRevert {
 	public static void main(String[] args) {
 		;
 		System.out.println("erstePerson lernt rebease");
+		System.out.println("erstePerson lernt rebease wieder");
 		
 	}
 
