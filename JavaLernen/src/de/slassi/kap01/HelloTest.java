889a783a78618219026b488f76330abe01b90228
Slassi <mehdi_slassi@gmx.de>
2020-08-05 21:46:52 +0200

    neuekommitaufmasterneu

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -5,7 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		
 		System.out.println("neue Kommit auf master");
-		System.out.println("neue Kommit auf master nach abbranch");
+		System.out.println("neue Kommit auf masterneu nach abbranch");
 	}
 
 }
