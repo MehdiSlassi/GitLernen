495c99d1e0df9622204518f2cd1fb73e6d16e47e
Slassi <mehdi_slassi@gmx.de>
2020-08-07 13:57:39 +0200

    while and if introduced

--- HeadFirstbook/src/com/slassi/chapter1/MyFirstClass.java
+++ HeadFirstbook/src/com/slassi/chapter1/MyFirstClass.java
@@ -5,6 +5,19 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("This is my first class");
 		System.out.println("java is funny");
+		// while loop
+		int x = 1;
+
+		while (x < 4) {
+			System.out.println("x ist: " + x);
+			x = x + 1;
+		}
+
+		if (x == 1) {
+			System.out.println("x muss 1 sein");
+		} else {
+			System.out.println("der Wert von x ist: " + x);
+		}
 
 	}
 
