165466e577e6ec60a7f7f90a1e7b734abadbbc06
Slassi <mehdi_slassi@gmx.de>
2020-08-11 18:28:14 +0200

    kommit2slassi

--- HeadFirstbook/src/com/slassi/chapter1/KlasseSlassi.java
+++ HeadFirstbook/src/com/slassi/chapter1/KlasseSlassi.java
@@ -4,7 +4,7 @@ public class KlasseSlassi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("erste Kommit1");
+		System.out.println("erste Kommit_slassi1");
 	}
 
 }
