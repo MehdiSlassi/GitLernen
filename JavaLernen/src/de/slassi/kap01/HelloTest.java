0a1486e104cb5b044bdf949513d4b667ab91d1ef
Slassi <mehdi_slassi@gmx.de>
2020-08-11 19:14:51 +0200

    zweites Kommit auf Msater

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -3,11 +3,9 @@ package de.slassi.kap01;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		
-		
-		
+	
 		System.out.println("erste kommit auf master");
-		
+		System.out.println("zweites kommit auf master");
 	}
 
 }
