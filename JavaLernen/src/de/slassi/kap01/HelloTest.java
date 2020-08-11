15a861ba1e4798f41f3ac2e86cada9e9919eb928
Slassi <mehdi_slassi@gmx.de>
2020-08-11 18:41:48 +0200

    erstekommitslassi

--- JavaLernen/src/de/slassi/kap01/HelloTest.java
+++ JavaLernen/src/de/slassi/kap01/HelloTest.java
@@ -5,9 +5,9 @@ public class HelloTest {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("neue Kommit auf masterneu nach abbranch");
-		System.out.println("kommit1neumaster");
-		System.out.println("kommit2neumaster");
+		System.out.println("erste kommit von slassi");
+		
+	
 	}
 
 }
